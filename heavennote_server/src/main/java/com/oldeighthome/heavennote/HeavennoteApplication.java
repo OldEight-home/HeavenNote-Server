9f923513d4001232e6849436b9eac11a2b31e2f4
YY-nb <916295483@qq.com>
2021-11-20 22:31:02 +0800

    first commit

---
+++ heavennote_server/src/main/java/com/oldeighthome/heavennote/HeavennoteApplication.java
@@ -0,0 +1,14 @@
+package com.oldeighthome.heavennote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HeavennoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeavennoteApplication.class, args);
+    }
+
+}
